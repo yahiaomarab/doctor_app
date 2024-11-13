04bac9eaed8348c862d18170d07a351c6dd2f814
yahiaomarab <yahiaomar746@gmail.com>
2024-11-13 03:23:48 +0200

    -solve issues of gradle -solve issues of java version -solve issues related to splash after android 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:doctor_app/core/routing/app_router.dart';
-import 'package:doctor_app/core/routing/routes.dart';
-import 'package:doctor_app/core/theming/colors.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:doctor_app/doctor_app.dart';
+import 'package:flutter/material.dart';
 
-class DoctorApp extends StatelessWidget {
-  final AppRouter appRouter;
-  const DoctorApp({super.key, required this.appRouter});
-
-  @override
-  Widget build(BuildContext context) {
-    return  ScreenUtilInit(
-      designSize: const Size(375,812),
-      minTextAdapt: true,
-      child:MaterialApp(
-        title: "Doctor App",
-        theme: ThemeData(
-          primaryColor: ColorsManager.mainBlue,
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        onGenerateRoute: appRouter.generateRoute,
-        initialRoute: Routes.onBoardingScreen,
-        debugShowCheckedModeBanner: false,
-      )
-    );
-  }
+void main(){
+  runApp( DoctorApp(
+    appRouter:AppRouter()
+  ));
 }
